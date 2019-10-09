8089e8348d026d289d46c2d7b939ad85bdfbf9fb
ChibiMuse <megan.smith@live.com>
2019-10-08 18:57:34 -0700

    adds math library and comments
    I added the math library to use pi and also added comments to describe each part of the program.

--- pizzacostperinch.py
+++ pizzacostperinch.py
@@ -1,24 +1,30 @@
 #pizza_cost.py
 #by Megan Smith
 #A simple program that calculates the cost per square inch of pizza.
+#imported library:
+from math import pi
 
-
+#main function the describes the program being called
 def main():
     print("This program calculates the cost per square inch of pizza!")
     pizza_cost()
 
 def pizza_cost():
-    pie = 3.14159
     try:
+        #input
         pizzaSize = int(input("Enter the diameter of the pizza (in inches):"))
         pizzaCost = float(input("Enter the cost of the pizza (in dollars):"))
+        #process the radius
         radius = pizzaSize/2
-        area = pie * (radius*radius)
+        #process the area
+        area = pi * (radius*radius)
+        #process the cast per sq inch
         costPer = round(pizzaCost/area, 2)
+        #output
         print("The", str(pizzaSize),"inch pizza cost ${:.2f}".format(round(pizzaCost,2))+".")
         print("Cost per square inch: ${:.2f}".format(round(costPer,2))+"/inch")
         input("Press <Enter> to end")
-        
+     #catches any errors and has the user try again.   
     except:
         print("Please enter a whole number without other characters.")
         pizza_cost()
